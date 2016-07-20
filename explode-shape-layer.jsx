8ed4ac543dbc64617a53ee7c0ba27689dae96805
aturtur <rautio.arttu@gmail.com>
2016-07-21 00:49:35 +0300

    added script

---
+++ explode-shape-layer.jsx
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////////////
+// explode shape layer
+// https://twitter.com/aturtur
+////////////////////////////////////////////////////////////////////////////////////////
+// desc: puts each shape to own layer
+////////////////////////////////////////////////////////////////////////////////////////
+app.beginUndoGroup("Explode shape layer");
+
+var comp = app.project.activeItem;
+var layer = comp.selectedLayers[0];
+var shapeContents = layer.property("ADBE Root Vectors Group");
+var x, y, d;
+
+for (var i = 1; i <= shapeContents.numProperties; i++) { 
+    
+        layer.duplicate();
+        theDuplicate = comp.layer(layer.index - 1);
+        theDuplicate.name = theDuplicate.property("ADBE Root Vectors Group").property(i).name;
+        x = 0;
+        y = 0;
+        d = 0;
+        
+        for (var h = 1; h < i; h++) {
+            theDuplicate.property("ADBE Root Vectors Group").property(h - x).remove();
+            x++;
+            d++;
+        }
+    
+        for (var k = 2; k <= (shapeContents.numProperties - d ); k++) {
+            theDuplicate.property("ADBE Root Vectors Group").property(k - y).remove();
+            y++;
+        }
+}
+layer.enabled = false;
+app.endUndoGroup();
