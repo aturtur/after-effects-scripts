1b50de0dbbd95c0708d87d81e095ea988441577f
aturtur <rautio.arttu@gmail.com>
2016-07-22 17:48:03 +0300

    stuff

--- parent-above.jsx
+++ parent-above.jsx
@@ -1,10 +1,8 @@
 app.beginUndoGroup("Parent above");
 
 var comp = app.project.activeItem;
-
 for (var i = 0; i < comp.selectedLayers.length; i++){
 	curLayer = comp.selectedLayers[i];
 	curLayer.parent = comp.layer(curLayer.index-1);
 }
-
 app.endUndoGroup();
