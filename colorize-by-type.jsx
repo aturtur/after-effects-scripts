cd297698ea63fd9760612c4b7b714794e7151f68
Arttu Rautio <rautio.arttu@gmail.com>
2017-03-20 21:28:35 +0200

    update

--- colorize-by-type.jsx
+++ colorize-by-type.jsx
@@ -23,44 +23,68 @@ var sandstone= 15;
 var darkgreen= 16;
 
 // layers
-var shapelayer      = blue;
-var textlayer       = orange;
-var cameralayer     = purple;
-var lightlayer      = yellow;
-var avlayer         = green;
-var solidlayer      = red;
 var adjustmentlayer = seafoam;
+var avlayer         = green;
+var audiolayer      = green;
+var cameralayer     = purple;
+var complayer       = cyan;
+var guidelayer      = none;
+var lightlayer      = yellow;
 var nulllayer       = none;
+var shapelayer      = blue;
+var solidlayer      = red;
+var textlayer       = orange;
 
 // do it
 for (var i = 1; i <= layers; i++) {
-    if (comp.layer(i) instanceof AVLayer) {
+
+    if (comp.layer(i) instanceof AVLayer) {       
         comp.layer(i).label = avlayer;
+
+        if (!comp.layer.hasVideo) {
+            comp.layer(i).label = audiolayer;
+        }
+        
         if (comp.layer(i).source.mainSource instanceof SolidSource) {
-                comp.layer(i).label = solidlayer;
-            }
-      if (comp.layer(i).nullLayer == true) {
-        comp.layer(i).label = nulllayer;
-      }      
-            if (comp.layer(i).adjustmentLayer == true) {
-         comp.layer(i).label = adjustmentlayer;
-      }
+            comp.layer(i).label = solidlayer;
+        }
+        
+        if (comp.layer(i).nullLayer == true) {
+            comp.layer(i).label = nulllayer;
+        }      
+        
+        if (comp.layer(i).adjustmentLayer == true) {
+             comp.layer(i).label = adjustmentlayer;
+        }
+
+        if (comp.layer(i).source instanceof CompItem) {
+            comp.layer(i).label = complayer;
+        }
     }
+
     if (comp.layer(i) instanceof ShapeLayer)
     {
         comp.layer(i).label = shapelayer;
     }
+
     if (comp.layer(i) instanceof TextLayer)
     {
         comp.layer(i).label = textlayer;
     }
+
     if (comp.layer(i) instanceof CameraLayer)
     {
         comp.layer(i).label = cameralayer;
     }
+
     if (comp.layer(i) instanceof LightLayer)
     {
         comp.layer(i).label = lightlayer;
+    }
+
+    if (comp.layer(i).guideLayer)
+    {
+        comp.layer(i).label = guidelayer;
     }
 }
 
--- explode-shape-layer.jsx
+++ explode-shape-layer.jsx
@@ -1,31 +1,53 @@
-app.beginUndoGroup("explode shape layer");
+app.beginUndoGroup("Explode shape layer");
 
-var comp = app.project.activeItem;
-var layer = comp.selectedLayers[0];
-var shapeContents = layer.property("ADBE Root Vectors Group");
+var thisComp = app.project.activeItem;
+var theLayer = thisComp.selectedLayers[0];
+var shapeContents = theLayer.property("ADBE Root Vectors Group");
 var x, y, d;
 
-for (var i = 1; i <= shapeContents.numProperties; i++) { 
-    
-        layer.duplicate();
-        theDuplicate = comp.layer(layer.index - 1);
+if (theLayer.selectedProperties.length == 0) {
+    // if only layer is selected
+    for (var i = 1; i <= shapeContents.numProperties; i++) {         
+        theLayer.duplicate();
+        theDuplicate = thisComp.layer(theLayer.index - 1);
         theDuplicate.name = theDuplicate.property("ADBE Root Vectors Group").property(i).name;
-        theDuplicate.label = layer.label;
-        theDuplicate.enables = true;
+        x = 0;
+        y = 0;
+        d = 0;        
+        for (var h = 1; h < i; h++) {
+            theDuplicate.property("ADBE Root Vectors Group").property(h - x).remove();
+            x++;
+            d++;
+        }    
+        for (var k = 2; k <= (shapeContents.numProperties - d ); k++) {
+            theDuplicate.property("ADBE Root Vectors Group").property(k - y).remove();
+            y++;
+        }
+    }
+} else if (theLayer.selectedProperties.length > 0) {
+    // if properties are selected
+    for (var i = 0; i < theLayer.selectedProperties.length; i++) {
+        theLayer.duplicate();
+        theDuplicate = thisComp.layer(theLayer.index - 1);
+        theDuplicate.name = theLayer.selectedProperties[i].name;
         x = 0;
         y = 0;
         d = 0;
         
         for (var h = 1; h < i; h++) {
-            theDuplicate.property("ADBE Root Vectors Group").property(h - x).remove();
-            x++;
-            d++;
+            if (theDuplicate.property("ADBE Root Vectors Group").property(h - x).name != theLayer.selectedProperties[i].name) {
+                theDuplicate.property("ADBE Root Vectors Group").property(h - x).remove();
+                x++;
+                d++;
+            }
+        }    
+        for (var k = 1; k <= (shapeContents.numProperties - d ); k++) {
+            if (theDuplicate.property("ADBE Root Vectors Group").property(k - y).name != theLayer.selectedProperties[i].name) {
+                theDuplicate.property("ADBE Root Vectors Group").property(k - y).remove();
+                y++;
+            }
         }
-    
-        for (var k = 2; k <= (shapeContents.numProperties - d ); k++) {
-            theDuplicate.property("ADBE Root Vectors Group").property(k - y).remove();
-            y++;
-        }
+    }
 }
-layer.enabled = false;
+theLayer.enabled = false;
 app.endUndoGroup();
